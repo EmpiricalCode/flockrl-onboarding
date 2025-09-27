b99f8ea200165f0262710fa921b972ef7eabb958
empiricalcode <daniellongschool@gmail.com>
2025-09-26 23:00:11 -0400

    feat: decrease max episode steps to prevent hovering

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 
 from stable_baselines3 import PPO
 
-env = gym.make("LunarLander-v3")
+env = gym.make("LunarLander-v3", max_episode_steps=300)
 
 # Create and train the agent
 model = PPO("MlpPolicy", env, verbose=1)
