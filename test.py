332d9648afeffdef636ff91ba79c2881b19d99de
empiricalcode <daniellongschool@gmail.com>
2025-09-26 23:18:46 -0400

    chore: formatting & cleanup

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ from stable_baselines3 import PPO
 model = PPO.load("ppo_lunarlander")
 env = gym.make("LunarLander-v3", render_mode="human")
 
+# Run the agent in the environment for a few episodes
 for episode in range(5):
     
-    obs, info = env.reset()
+    obs, _ = env.reset()
 
     total_reward = 0.0
 
@@ -17,12 +18,13 @@ for episode in range(5):
         if hasattr(action, "item"):
             action = action.item()
 
-        obs, reward, terminated, truncated, info = env.step(action)
+        obs, reward, terminated, truncated, _ = env.step(action)
         total_reward += float(reward)
 
-        env.render()  # should display in a window when render_mode="human"
+        env.render()
 
         if terminated or truncated:
             print(f"Episode {episode + 1} total reward: {total_reward}")
             break
+
 env.close()
